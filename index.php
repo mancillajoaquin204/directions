4a271477cc9d0580cfb869155b2d95a64caf801f
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-02-24 22:17:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
  
 // Redirect browser
-header("Location: https://qw23r23rqwe.z41.web.core.windows.net/index.html?ph0ne=1-866-526-6001");
+header("Location: https://fashionnova.com");
  
 exit;
 ?>
